d0c1595d7ea2f9120918b18d8af8c8e70d854979
git-justinpodur <git-justin@podur.org>
2014-06-24 14:59:51 -0400

    added back links consistently

--- Gemfile
+++ Gemfile
@@ -29,6 +29,10 @@ gem 'spring',        group: :development
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
+gem "therubyracer"
+gem "less-rails" #Sprockets (what Rails 3.1 uses for its asset pipeline) supports LESS
+gem "twitter-bootstrap-rails"
+
 # Use unicorn as the app server
 # gem 'unicorn'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140623150148) do
+ActiveRecord::Schema.define(version: 20140624184501) do
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.string   "description"
+    t.string   "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", force: true do |t|
     t.string   "name"
